9af47b0f96e9edfc1e10051abd54c59e2aa3b610
victor-sc12 <victorsuquilanda8639@outlook.com>
2025-12-22 23:15:29 -0500

    Se adicionaron update y delete views para instancias de álbum. Se definierón grupos de usuarios con permisos específicos, se gestionó las views con respecto a estos permisos.

--- apps/musica/views.py
+++ apps/musica/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+
+@login_required
 def artists_view(request):
     artistas = Artista.objects.prefetch_related('albums')
 
@@ -11,6 +14,8 @@ def artists_view(request):
 
     return render(request, 'musica/artists.html', {'artistas':artistas})
 
+@login_required
+@permission_required('musica.add_album', raise_exception=True)
 def add_album(request, slug):
 
     artista = Artista.objects.get(slug = slug)
@@ -25,7 +30,35 @@ def add_album(request, slug):
     
     return render(request, 'musica/add_album.html', context={'form':form})
 
+@login_required
 def album_detail(request, slug):
     album = Album.objects.prefetch_related('canciones').get(slug = slug)
     canciones = album.canciones.all()
     return render(request, 'musica/album_detail.html', {'album':album, 'canciones':canciones})
+
+@login_required
+@permission_required('musica.change_album', raise_exception=True)
+def album_update(request, slug):
+    album = Album.objects.get(slug = slug)
+
+    if request.method == 'POST':
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('detail_album', slug=slug)
+    
+    else:
+        form = AlbumForm(instance=album)
+    
+    return render(request, 'musica/album_update.html', {'form':form, 'album':album})
+    
+@login_required
+@permission_required('musica.delete_album', raise_exception=True)
+def album_delete(request, slug):
+    album = Album.objects.get(slug = slug)
+
+    if request.method == 'POST':
+        album.delete()
+        return redirect('artists_view')
+    
+    return render(request, 'musica/album_delete.html', {'album': album})
