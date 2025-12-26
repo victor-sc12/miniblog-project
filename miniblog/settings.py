204829f0cbdb7528c9a4e4beb2aa7409ab076e0c
victor-sc12 <victorsuquilanda8639@outlook.com>
2025-12-26 15:18:19 -0500

    Se implementarón modificaciones automáticas en algunos archivos del proyecto, como migraciones y en caché de apps, debido a cambio de versión de python.

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_sass',
+    # 'django_sass', 
     'apps.blog',
     'apps.musica',
     'apps.profiles',
